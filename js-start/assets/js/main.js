a17ec5d7cb27e0a8655afe05a3574602a68f8e1a
User <7b4s8zl@code.edu.az>
2023-04-12 20:07:42 +0400

    added file

---
+++ js-start/assets/js/main.js
@@ -0,0 +1,147 @@
+// var num = 60
+// if (num%30==0)
+//  {
+//     alert("eded 3 e ve 10 a bolunur")
+// }
+// else{
+//     alert("eded 3e ve 10 a bolunmur")
+// }
+
+
+
+
+
+
+
+//  var num=45
+//  if (num>90 && num<101 ) 
+//  {
+//     alert("A")
+//  } 
+//  else if (num>80 && num<91)
+//  {
+//     alert (" B")
+//  }
+//  else if (num>70 && num<81) 
+//  {
+//     alert("C")
+//  } 
+//  else if (num>60 && num<71)
+//  {
+//     alert (" D")
+//  }
+//  else if (num>50 && num<61) 
+//  {
+//     alert("E")
+//  } 
+//  else if(num >0 && num < 50){
+//     alert("kesildiniz")
+//  }
+//  else
+//  {
+//     alert (" duzgun deyer daxil edin ")
+//  }
+
+
+
+
+
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+
+
+// var x=6 , y=6;
+// if (x%2==0 && y%2==0) 
+// {
+//     alert(x+y)
+// }
+// else{
+//     alert("false")
+// }
+
+
+// var num=98
+// if(num>9 && num<100){
+//     var teklik=num%10
+//     var onluq=(num-teklik)/10
+//     if(teklik>onluq){
+//         alert("teklik boyukdur")
+//     }
+//     else{
+//         alert("onluq boyukdur")
+//     }
+// }
+
+
+
+
+// verilmis 2 reqemli ededin reqemlerinin bir birinden ferqli olub olmadigini tapin
+
+// var num=67
+// if (num>9 && num<100) {
+//  var teklik=num%10
+//  var onluq=(num-teklik)/10
+//  if(teklik!=onluq){
+//     alert("teklik onluga beraber deyil")
+//  }
+//  else{
+//     alert("teklik onluga beraberdir")
+// }
+// }
+// else{
+//     alert("iki reqemli deyil")
+// }
+
+
+
+// verilmis 4 reqemli ededin mertebelerinin cemini tapin
+ 
+// var num=prompt('reqemi daxil edin')
+// var teklik=num%10
+  
+// var onluq=((num-teklik)%100)/10
+
+// var yuzluk=((num-num%100)/100)%10
+
+// var minlik=((num-num%1000))/1000
+
+// if(num>999 && num<10000){
+   
+// alert(teklik+onluq+yuzluk+minlik)
+    
+// }
+// else{
+//     alert("4 reqemli deyil")
+// }
+
+
+// 
+
+
+
+// 1den 100e qeder cut ededlerin cemini tapan proqram yazin   for while ve ya for while
+
+// var sum = 0;
+// for(let i = 0; i <=100; i++){
+//     if(i % 2 == 0){
+//         //sum = sum + 1
+//         sum += i
+//     }
+// }
+
+// alert(sum)
+
+
+// eger cemini deyil de hasilini sorusarsa onda var multi=1 en sonda da alert(multi olacaq)
